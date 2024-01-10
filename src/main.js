c274c79195e53d384c27fa4750b59ae376ddb001
victorgaenshirt <vgaenshirt@posteo.de>
2024-01-10 14:25:07 +0100

    turned app into a PWA

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from "@/router";
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.js"
 import "bootstrap-icons/font/bootstrap-icons.css"
+import './registerServiceWorker'
 
 const app = createApp(App)
 app.use(Quasar, quasarUserOptions, { config: {} })
